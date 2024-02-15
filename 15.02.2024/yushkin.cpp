a3c0eb8abde771f6eb5754930d24aa20667084ea
rugliko <159914751+rugliko@users.noreply.github.com>
2024-02-15 18:08:59 +0300

    Add files via upload

---
+++ 15.02.2024/yushkin.cpp
@@ -0,0 +1,113 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, " ");
+	float a, b, c;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cout << "Вычисление обьёма параллелепипеда." << endl << "Введите данные" << endl;
+	cout << "длинна (см) - >" << a << endl;
+	cout << "ширина (см) - >" << b << endl;
+	cout << "высота (см) - >" << c << endl;
+	cout << " Обьём:" << a * b * c << "куб.см.";
+	int a;
+	cin >> a;
+	if (a / 2 != 0) {
+		cout << "odd";
+	}
+	else { cout << "even"; }
+     int a, b;
+	cin >> a;
+	cin >> b;
+	cout << ((a > b) ? a  : b);
+	int a;
+	cin >> a;
+	if (a > 0) {
+		cout << "+";
+	}
+	if (a = 0) {
+		cout << "  =";
+	}
+	if (a < 0) {
+		cout << "-";
+	}
+	int a, b;
+	cin >> a >> b;
+		cout << a<<endl << b;
+	}
+	else{
+		cout << b<<endl << a;
+	}	
+	int a, b, c, d, f;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cin >> d;
+	cin >> f;
+	if ((a + b + c + d + f) / 5 > 4) {
+		cout << "допуск";
+	}
+	else {
+		cout << "недопуск";
+	double a;
+     cin >> a;
+	if (a / 2 != 0) {
+		cout << a / 2;
+
+	}
+	else {
+		cout << a * 3;
+		
+	}
+	double a, b;
+	char sign;
+	cin >> a >> b;
+	cout << a << sign << b;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
